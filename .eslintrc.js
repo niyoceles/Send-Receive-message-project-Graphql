be6b82d20341fea80c97e54837835c4f8221480e
niyoceles <niyoceles3@gmail.com>
2019-12-31 16:20:31 +0200

    setup configuration

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: 'airbnb-base',
+  plugins: ['import'],
+  rules: {
+    'linebreak-style': 0,
+    'comma-dangle': ['error', 'never']
+  }
+};
---
+++ models/index.js
@@ -0,0 +1,21 @@
+import Sequelize from 'sequelize';
+
+const sequelize = new Sequelize('chatting_db', 'postgres', 'admin', {
+  host: 'localhost',
+  dialect: 'postgres'
+});
+
+const db = {
+  User: sequelize.import('./user')
+};
+
+// Object.keys(db).forEach((modelName) => {
+//   if ('associate' in db[modelName]) {
+//     db[modelName].associate(db);
+//   }
+// });
+
+db.sequelize = sequelize;
+// db.Sequelize = Sequelize;
+
+export default db;
